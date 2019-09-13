b5f7cbf17abab32b0922afed84ec5c83e368947c
Emile Belanger <emile.belanger@gmail.com>
2019-09-13 19:32:01 +0100

    Can now block each gamepad stick separately

--- mobile/game_interface.c
+++ mobile/game_interface.c
@@ -385,21 +385,25 @@ void IN_Move_Android (usercmd_t *cmd)
 	}
 
     int blockGamepad( void );
-    if( blockGamepad() )
-        return;
+    int blockMove = blockGamepad() & ANALOGUE_AXIS_FWD;
+    int blockLook = blockGamepad() & ANALOGUE_AXIS_PITCH;
 
-	cmd->forwardmove += forwardmove * cl_forwardspeed.value * 2; //Always run! (x2)
-	cmd->sidemove  += sidemove   * cl_sidespeed.value * 2;
+    if( !blockMove )
+    {
+	    cmd->forwardmove += forwardmove * cl_forwardspeed.value * 2; //Always run! (x2)
+	    cmd->sidemove  += sidemove   * cl_sidespeed.value * 2;
+    }
 
+    if( !blockLook )
+    {
+        cl.viewangles[PITCH] += -look_pitch_mouse * 300;
+        look_pitch_mouse = 0;
+        cl.viewangles[PITCH] += look_pitch_joy * 6 * (host_frametime * 1000.f / 16.f); // Presume was scaled at 60FPS;;
 
-	cl.viewangles[PITCH] += -look_pitch_mouse * 300;
-	look_pitch_mouse = 0;
-	cl.viewangles[PITCH] += look_pitch_joy * 6 * (host_frametime * 1000.f / 16.f); // Presume was scaled at 60FPS;;
-
-
-	cl.viewangles[YAW] += look_yaw_mouse * 400;
-	look_yaw_mouse = 0;
-	cl.viewangles[YAW] += look_yaw_joy * 6 * (host_frametime * 1000.f / 16.f); // Presume was scaled at 60FPS;;
+        cl.viewangles[YAW] += look_yaw_mouse * 400;
+        look_yaw_mouse = 0;
+        cl.viewangles[YAW] += look_yaw_joy * 6 * (host_frametime * 1000.f / 16.f); // Presume was scaled at 60FPS;;
+    }
 
 	V_StopPitchDrift();
 }
